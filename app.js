75387f1f02737fff9cde1281189098d3e0ae63ca
Tanja Rietveldt <diafox2015@gmail.com>
2020-03-15 14:28:09 +0100

    add contact form and buttom to the top

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ back.on('click',function(){
     }
 )
 
+const containerLeft = $(".containerLeft");
+const photoL = $(".imgLeft");
+const lettersL = $(".lettersLeft");
+
+containerLeft.hover(function(){
+    photoL.toggleClass('change');
+    lettersL.toggleClass('visible');
+})
 const container = $(".container");
 const photo = $(".img");
 const letters = $(".letters");
@@ -18,3 +26,31 @@ container.hover(function(){
     photo.toggleClass('change');
     letters.toggleClass('visible');
 })
+const containerRight = $(".containerRight");
+const photoR = $(".imgRight");
+const lettersR = $(".lettersRight");
+
+containerRight.hover(function(){
+    photoR.toggleClass('change');
+    lettersR.toggleClass('visible');
+})
+
+// button to the topFunction//Get the button
+var toTop = document.getElementById("toTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTop.style.display = "block";
+  } else {
+    toTop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
